3010b4b3ac614e334094e04260270c4fc578e8e0
Sasit Srirat <55717534+sasitsrirat@users.noreply.github.com>
2022-11-02 20:51:42 +0700

    Add files via upload

---
+++ Project3_xxxxxxx/project3/src/main/java/project3/App.java
@@ -0,0 +1,25 @@
+package project3;
+
+import java.awt.*;
+import java.io.*;
+import java.util.*;
+import javax.swing.*;
+import java.lang.*;
+
+/**
+ * Hello world!
+ */
+
+
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
