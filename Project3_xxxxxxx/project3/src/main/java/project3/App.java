ecd7cdd103d8f1f325050984bdb0cd48640d94cb
musiczu <ravipol.cha2545@gmail.com>
2022-11-02 23:56:25 +0700

    Merge branch 'main' of https://github.com/sasitsrirat/project3

--- Project3_xxxxxxx/project3/src/main/java/project3/App.java
+++ Project3_xxxxxxx/project3/src/main/java/project3/App.java
@@ -16,6 +16,6 @@ public final class App {
     }
 
     public static void main(String[] args) {
-        System.out.printf("Hello World!\nI am Kawin Sudloh");
+        System.out.printf("Hello World!\nI am Kawin Sudloh ,dfsdf");
     }
 }
