200e15f9a20b1433be5d54fb4766b222dc1fc591
Kawin Keng <kawin.ken@student.mahidol.ac.th>
2022-11-03 07:42:32 +0700

    change location file

---
+++ Project3_xxxxxxx/project3/src/main/java/project3/Character.java
@@ -0,0 +1,8 @@
+package project3;
+public class Character {
+    protected int HP = 0;
+    protected int ATK = 0;
+    protected int DEF = 0;
+    protected int SP = 0;
+    protected int SPD = 0;
+}
