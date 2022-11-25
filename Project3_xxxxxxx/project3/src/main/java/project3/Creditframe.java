ad3594431b8bbcf4956fb6e112d6ff8750ac470f
Sasit Srirat <55717534+sasitsrirat@users.noreply.github.com>
2022-11-25 17:39:48 +0700

    add and fix credit frame

--- Project3_xxxxxxx/project3/src/main/java/project3/Creditframe.java
+++ Project3_xxxxxxx/project3/src/main/java/project3/Creditframe.java
@@ -3,42 +3,79 @@ package project3;
 import javax.swing.JFrame;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
+
 import javax.swing.*;
 
 public class Creditframe extends JFrame 
 {
+    protected ArrayList<Sound> musicSound;
+    protected ArrayList<Sound> effectSound;
+    protected String path;
     protected JPanel creditPanel;
     protected JLabel contentPane;
-    //JPanel credit = new JPanel();
+    protected int frameWidth = 1366;
+    protected int frameHeight = 768;
+
     //constructor
-    public Creditframe() {
-        String imagepath = "Project3_xxxxxxx/project3/src/pictures/";
+    public Creditframe(ArrayList<Sound> mSound, ArrayList<Sound> eSound, String imagepath) {
+        musicSound = mSound;
+        effectSound = eSound;
+        path = imagepath;
+
         setTitle("Credit");
-	    setBounds(300, 200, 1366, 768);
+	    setBounds(50, 50, frameWidth, frameHeight);
 	    setVisible(true);
+        setResizable(false);
 	    setDefaultCloseOperation( WindowConstants.DISPOSE_ON_CLOSE );
+
         creditPanel = new JPanel();
         //JFrame frame = new JFrame();
         creditPanel.setBounds(50,100,1366,768);
         creditPanel.setLayout(null); //set layout
         
-        ImageIcon imageIcon = new ImageIcon(imagepath + "ground.jpg");
-        imageIcon.setImage(imageIcon.getImage().getScaledInstance(1366, 768, Image.SCALE_DEFAULT));
+        
+        
+        ImageIcon imageIcon = new ImageIcon(path + "cityrobot.gif");
+        imageIcon.setImage(imageIcon.getImage().getScaledInstance(frameWidth, frameHeight, Image.SCALE_DEFAULT));
         contentPane = new JLabel();
         setContentPane(contentPane = new JLabel());
-        contentPane.setIcon(imageIcon);
+        
         setContentPane(contentPane);
         
         JPanel panel = new JPanel();
         {
             JLabel text_1 = new JLabel("Credit");
             JLabel text_2 = new JLabel("MR.XXAABB XXAABB 1234556");
+            text_2.setFont(new Font("Charter", Font.BOLD, 30));
+            text_2.setForeground(Color.white);
             panel.add(text_1);
             panel.add(text_2);
             validate();
 
         }
-     
+        JButton backButton = new JButton("Back");
+        {
+            backButton.setBounds(1000, 630, 100, 50);
+            contentPane.add(backButton);
+            backButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent event) {
+                    String button = event.getActionCommand();
+                    if (button.equals("Back")) {
+                        dispose();
+                        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+                    }
+                }
+            });
+        }
+        contentPane.setIcon(imageIcon);
+        contentPane.add(panel);
+        repaint();
+        
+        creditPanel.setVisible(true);
+        panel.setVisible(true);
+        
         
          
     }
